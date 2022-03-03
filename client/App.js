9459f89c85b1b0b3f5e174247e8d131320e80561
tmg2549 <tmg2549@utexas.edu>
2022-03-02 23:16:55 -0600

    Integrate tailwind css

--- client/App.js
+++ client/App.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const App = () => ( 
     <div>
-        <h1 className="background">Hello React</h1>
+        <h1 className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">Hello React</h1>
         <h2>How are you?</h2>
         <h3>Doing well!</h3>
     </div>
