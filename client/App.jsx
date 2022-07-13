01138dcf54a9e63bb54861773ba1a017ba46658f
tmg2549 <tmg2549@utexas.edu>
2022-07-12 20:48:01 -0500

    Fix compilation error

--- client/App.jsx
+++ client/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, NavLink, HashRouter } from "react-router-dom";
 import NavbarContainer from "./containers/NavbarContainer";
 // import OverviewContainer from "./containers/OverviewContainer";
-import Counter from "./features/counter/Counter.js"
+// import Counter from "./features/counter/Counter.js"
 
 const App = () => ( 
     // <HashRouter>
